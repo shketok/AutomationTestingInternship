c926ec6698e80dfc2e51c63e0aacb5afaa6c2151
artem <a.lebakov@a1qa.com>
2021-08-23 18:39:58 +0300

    initial

--- src/main/java/elevator/exceptions/DoorsAreOpenException.java
+++
@@ -1,8 +0,0 @@
-package elevator.exceptions;
-
-public class DoorsAreOpenException extends RuntimeException {
-
-    public DoorsAreOpenException() {
-        super("Impossible to go to the destination with open doors");
-    }
-}
--- src/main/java/elevator/exceptions/FloorOutOfBoundException.java
+++
@@ -1,8 +0,0 @@
-package elevator.exceptions;
-
-public class FloorOutOfBoundException extends RuntimeException {
-
-    public FloorOutOfBoundException(int destinationFloor) {
-        super(String.format("Destination floor %d is out of bound of preset min and max floors", destinationFloor));
-    }
-}
